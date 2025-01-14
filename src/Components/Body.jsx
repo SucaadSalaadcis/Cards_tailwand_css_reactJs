28d1fbb3703d527b75418e9b5eb04f1cddbdace8
Sucaad Salaad Osman <140339872+SucaadSalaadcis@users.noreply.github.com>
2025-01-14 13:33:29 +0300

    Update Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Body(props) {
   return (
     <div>
-      <div className="w-[400px] rounded-xl p-4 border border-red-950 mt-[40px] ">
+      <div className="w-[350px] rounded-xl p-4 border border-red-950 mt-[40px] ">
       <img src={props.image} alt="" className="w-full h-[300px] "/>
       <h1 className="font-bold">{props.title}</h1>
       <p>{props.desc}</p>
